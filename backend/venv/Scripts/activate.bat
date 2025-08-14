888edca553894e114dea3b9950e11ca9095381c6
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2025-08-14 10:35:15 +0530

    update dependencies

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Y4S1\Heat_Island\backend\venv
+set VIRTUAL_ENV=C:\Y4 S2\HeatScape\vlm-solution-service\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
